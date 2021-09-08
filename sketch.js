2cc89c437d4102ea7944b486634bae89f0232605
snawabi <snawabi2@csustan.edu>
2021-09-07 18:33:29 -0700

    2D ray casting

---
+++ sketch.js
@@ -0,0 +1,37 @@
+// 2D Ray Casting
+// https://editor.p5js.org/codingtrain/sketches/Nqsq3DFv-
+
+let walls = [];
+let ray;
+let particle;
+let xoff = 0;
+let yoff = 10000;
+
+function setup() {
+  resizeCanvas(windowWidth, windowHeight);
+  for (let i = 0; i < 20; i++) {
+    let x1 = random(width);
+    let x2 = random(width);
+    let y1 = random(height);
+    let y2 = random(height);
+    walls[i] = new Boundary(x1, y1, x2, y2);
+  }
+  walls.push(new Boundary(0, 0, width, 0));
+  walls.push(new Boundary(width, 0, width, height));
+  walls.push(new Boundary(width, height, 0, height));
+  walls.push(new Boundary(0, height, 0, 0));
+  particle = new Particle();
+}
+
+function draw() {
+  background('black');
+  for (let wall of walls) {
+    wall.show();
+  }
+  particle.update(noise(xoff) * width, noise(yoff) * height);
+  particle.show();
+  particle.look(walls);
+
+  xoff += 0.01;
+  yoff += 0.01;
+}
